988d58e8ac1156a2134ddd6066f7e80d2e0b4746
Aleksashka301 <aleksashka301@gmail.com>
2025-05-21 22:52:48 +0500

    Update models.py

--- apps/events/models.py
+++ apps/events/models.py
@@ -11,13 +11,22 @@ class Speaker(models.Model):
 
 class Event(models.Model):
     title = models.CharField(verbose_name='Название', max_length=100)
-    speakers = models.ManyToManyField('Speaker', related_name='events', verbose_name='Докладчики')
     description = models.TextField(verbose_name='Описание мероприятия', null=True, blank=True)
     start_event = models.DateTimeField(verbose_name='Начало мероприятия', null=True, blank=True)
     end_event = models.DateTimeField(verbose_name='Конец мероприятия', null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class SpeakerTalk(models.Model):
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE, verbose_name='Докладчики')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
+    start_performance = models.DateTimeField(verbose_name='Начало выступления', null=True, blank=True)
+    end_performance = models.DateTimeField(verbose_name='Конец выступления', null=True, blank=True)
+
+    def __str__(self):
+        return self.speaker.name
 
 
 
