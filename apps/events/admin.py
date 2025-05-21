0a2934922c17aa5af0acc3ab97b1b9290177aa2f
Aleksashka301 <aleksashka301@gmail.com>
2025-05-21 21:14:10 +0500

    Админ панель для двух таблиц

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Speaker, Event
+
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_speakers', 'start_event', 'end_event',)
+    filter_horizontal = ['speakers']
+
+    def get_speakers(self, obj):
+        return ", ".join([speaker.name for speaker in obj.speakers.all()])
+
+    get_speakers.short_description = 'Докладчики'
+
