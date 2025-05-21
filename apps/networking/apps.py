3ec47b6cf71f6a5ea6bcd7027e16dc43cd9f8f1f
Aleksashka301 <aleksashka301@gmail.com>
2025-05-21 21:13:13 +0500

    Дописал путь до приложения

--- apps/networking/apps.py
+++ apps/networking/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NetworkingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'networking'
+    name = 'apps.networking'
