e5d61df2e6fc95229598a2c104cef8e45d68a3c0
Aleksashka301 <aleksashka301@gmail.com>
2025-05-22 22:27:36 +0500

    Update models.py

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,14 +1,15 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Speaker(models.Model):
     name = models.CharField(verbose_name='Ф.И.О', max_length=100)
-    telegram_id = models.BigIntegerField(verbose_name='Telegram ID', unique=True, null=True, blank=True)
+    telegram_id = models.BigIntegerField(verbose_name='Telegram ID', null=True, blank=True)
     biography = models.TextField(verbose_name='Биография', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
 
     def __str__(self):
-        return self.name
+        return f'Спикер {self.name}'
 
 
 class Event(models.Model):
@@ -24,6 +25,7 @@ class Event(models.Model):
 class SpeakerTalk(models.Model):
     speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE, verbose_name='Докладчики')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
+    status = models.BooleanField(default=False, verbose_name='Статус выступления')
     start_performance = models.DateTimeField(verbose_name='Начало выступления', null=True, blank=True)
     end_performance = models.DateTimeField(verbose_name='Конец выступления', null=True, blank=True)
 
@@ -40,6 +42,9 @@ class UserProfile(models.Model):
     grade = models.TextField(verbose_name='Грейд', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
 
+    def __str__(self):
+      return f'Анкета {self.name}'
+
 
 class Question(models.Model):
     telegram_id = models.BigIntegerField(verbose_name='Telegram ID', null=True, blank=True)
@@ -49,3 +54,16 @@ class Question(models.Model):
     is_answered = models.BooleanField(default=False, verbose_name='Ответ')
     answer_text = models.TextField(verbose_name='Ответ', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+      return f'Вопрос от {self.name}'
+
+
+class Donate(models.Model):
+    telegram_id = models.BigIntegerField(verbose_name='Telegram ID', null=True, blank=True)
+    name = models.CharField(verbose_name='Ф.И.О', max_length=100)
+    amount = models.IntegerField(verbose_name='Сумма доната', validators=[MinValueValidator(1)])
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    def __str__(self):
+      return f'Донат от {self.name}'
